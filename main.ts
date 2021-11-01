76392a49b8965164e62b4ea235d7286f18ac0557
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-11-01 14:12:46 -0500

    reboot of new l337 program

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 function doInit () {
-    cn = 0
+    _const = 0
+    cnames = ["ursa minor", ""]
     consts = [[
     "d8",
     "h2",
@@ -8,8 +9,7 @@ function doInit () {
     "t8",
     "x6",
     "w1"
-    ]]
-    cname = ["Ursa Minor"]
+    ], ["", "", ""]]
     starpos = [
     "a",
     "b",
@@ -40,25 +40,9 @@ function doInit () {
     ]
 }
 input.onButtonPressed(Button.A, function () {
-    shoStars(consts[cn])
+    shoConst(_const)
 })
-function Star (text: string) {
-    pos = text.substr(0, 1)
-    mag = text.substr(1, 1)
-    spot = starpos.indexOf(pos)
-    led.plotBrightness(spot % 5, Math.trunc(spot / 5), 25 * parseFloat(mag))
-}
-input.onButtonPressed(Button.B, function () {
-    basic.showString("" + (cname[cn]))
-})
-let spot = 0
-let mag = ""
-let pos = ""
-let starpos: string[] = []
-let cname: string[] = []
-let cn = 0
-let consts: string[][] = []
-function shoStars (list: any[]) {
+function shoConst (cindx: number) {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -66,9 +50,24 @@ function shoStars (list: any[]) {
         . . . . .
         . . . . .
         `)
-    for (let value of list) {
-        Star(value)
+    for (let value of consts[_const]) {
+        doStar(value)
         basic.pause(100)
     }
 }
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (cnames[_const]))
+    shoConst(_const)
+})
+function doStar (star: string) {
+    spot = starpos.indexOf(star.substr(0, 1))
+    mag = parseFloat(star.substr(1, 1))
+    led.plotBrightness(spot % 5, Math.trunc(spot / 5), mag * 20)
+}
+let mag = 0
+let spot = 0
+let starpos: string[] = []
+let consts: string[][] = []
+let cnames: string[] = []
+let _const = 0
 doInit()
